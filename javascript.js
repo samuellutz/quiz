53b4361aab6712b7aeb92e2947c612b02efbf82b
samuel lutz <samlutz89@gmail.com>
2021-12-27 11:15:07 -0800

    adding questions

---
+++ javascript.js
@@ -0,0 +1,17 @@
+ 
+ 
+ var questions =  [
+     {
+         prompt: "What vitamin did sailors need to prevent scurvy?\N(a) vitamin c\N(b) vitamin A\N(c) vitamin B1\N(d) all of the above",
+         answer: "a"
+     },
+     {
+         prompt: "where do dolphins live?\N(a) a lake\N\(b)the river\N\(c)the sea\N\(d)space",
+         answer: "c"
+     },
+     {
+         prompt: "what is the third letter of the alphabet?\N(a) B\N\(b) C\n\(c) D\N\(d) Z",
+         answer: "b"
+     }
+ ]
+ var score = 0;

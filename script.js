32d8dbb37d9fc64be50c4eba3eadaeb1c1d5d628
samuel lutz <samlutz89@gmail.com>
2021-12-29 12:50:57 -0800

    getitng questions to cycle properly

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ const questionContainterElement = document.getElementById("question-container")
 const timeText = document.getElementById("time")
 const button1 = document.getElementById("btn1")
 const questionElement = document.getElementById("question")
-const answerButtons = document.getElementById("answer-buttons")
+const answerButtonsElement = document.getElementById("answer-buttons")
 let shuffledQuestions, currentQuestionIndex
 
 startbutton.addEventListener("click", startGame)
+nextbutton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 document.getElementById
 function startGame(){
     timer()
@@ -16,20 +20,64 @@ function startGame(){
     questionContainterElement.classList.remove("hide")
     timeText.classList.remove("hide")
     shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     setNextQuestion()
 }
 
 function setNextQuestion(questions) {
-    showQuestions(shuffledQuestions[currentQuestionIndex])
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function showQuestions(question){
-    questionElement.innerText = questions.question
-
+function showQuestion(question){
+    questionElement.innerText = question.question
+     question.answers.forEach(function (answers) {
+             const button = document.createElement("button");
+             button.innerText = answers.text;
+             button.classList.add("btn");
+             if (answers.correct) {
+                 button.dataset.correct = answers.correct;
+             }
+             button.addEventListener("click", selectAnswer);
+             answerButtonsElement.appendChild(button);
+         })
 }
 
-function selectAnswer(answer) {
+function resetState() {
+    clearStatusClass(document.body)
+    nextbutton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedbutton = e.target
+    const correct = selectedbutton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextbutton.classList.remove("hide")
+    }else {
+        startbutton.innerText = "restart"
+        startbutton.classList.remove("hide")
+    }
+}
+
+function setStatusClass (element, correct) {
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
 }
     function timer(){
     var counter = 80;
@@ -38,13 +86,12 @@ function selectAnswer(answer) {
        if (counter >= 0) {
           span = document.getElementById("time");
           span.innerHTML = counter;
-        //   window.location.reload();
        }
        else {
+        window.location.reload();
            alert ("sorry out of time.")
           clearInterval(counter);
         }
-        window.location.reload() 
         timeText.textContent = counter;
       }, 1000);
  };
@@ -59,20 +106,35 @@ const questions =  [
             {text: "Vitamin B1", correct: false},
             {text: "all of the above", correct: false},
         ]
+    },
+    {
+        question: "where do dolphins live?",
+       answers: [
+           {text: "a lake", correct: false},
+           {text: "a River", correct: false},
+           {text: "space", correct: false},
+           {text: "a sea", correct: true},
+       ]
+   },
+   {
+    question: "what is the third letter of the alphabet?",
+   answers: [
+       {text: "B", correct: false},
+       {text: "C", correct: true},
+       {text: "D", correct: false},
+       {text: "Z", correct: false},
+   ]
+    },
+    {
+        question: "what are your eyes for?",
+       answers: [
+           {text: "hearing", correct: false},
+           {text: "Taste", correct: false},
+           {text: "Smelling", correct: false},
+           {text: "Seeing", correct: true},
+       ]
     }
-]
+
+];
         
-//      {
-//          prompt: "where do dolphins live?\N(a) a lake\N\(b)the river\N\(c)space\N\(d)the sea",
-//          answer: "d"
-//      },
-//      {
-//          prompt: "what is the third letter of the alphabet?\N(a) B\N\(b) C\n\(c) D\N\(d) Z",
-//          answer: "b"
-//      },
-//      {
-//          prompt: "what are your eyes for? \N(a) Hearing \N\(b)taste\N\(c)touch\N\(d) to see",
-//          answer: "d"
-//      },
-//  ];
 //  we need for the prompt to pop up at random on the screen

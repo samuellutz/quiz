b89854b2b85aa64dbc0c918d31435368daa362d7
samuel lutz <samlutz89@gmail.com>
2021-12-30 12:32:22 -0800

    pushing scoreboard

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 var startbutton = document.getElementById("start-btn");
 var nextbutton = document.getElementById("next-btn");
+var enterhighScore = document.getElementById("enter-high-score")
 const questionContainterElement = document.getElementById("question-container")
 const timeText = document.getElementById("time")
 const button1 = document.getElementById("btn1")
 const questionElement = document.getElementById("question")
 const answerButtonsElement = document.getElementById("answer-buttons")
 let shuffledQuestions, currentQuestionIndex
+const endScreenElement = document.getElementById("end-screen")
+const submitButton = document.getElementById("submit-button")
+const scoreboardElement = document.getElementById("scoreboard")
+const resetButton = document.getElementById("reset-button")
+const restartButton = document.getElementById("restart-button")
+var highScorePage = document.getElementById("high-scores")
+var leaderBoard = document.getElementById("scores")
+var scores = []
+const finalScore = document.getElementById("score")
+var userName = document.getElementById("initials")
+const finishButton = document.getElementById("finish-button")
+
+function timer(){
+    var counter = 80;
+    setInterval(function() {
+      counter--;
+       if (counter >= 0) {
+          span = document.getElementById("time");
+          span.innerHTML = counter;
+       }
+       else {
+        window.location.reload();
+           alert ("sorry out of time.")
+          clearInterval(counter);
+        }
+        timeText.textContent = counter;
+      }, 1000);
+ };
 
 startbutton.addEventListener("click", startGame)
 nextbutton.addEventListener("click", () => {
@@ -13,16 +42,16 @@ nextbutton.addEventListener("click", () => {
     setNextQuestion()
 })
 document.getElementById
-function startGame(){
-    timer()
-    console.log("started")
-    startbutton.classList.add("hide")
-    questionContainterElement.classList.remove("hide")
-    timeText.classList.remove("hide")
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    setNextQuestion()
-}
+// function startGame(){
+//     timer()
+//     console.log("started")
+//     startbutton.classList.add("hide")
+//     questionContainterElement.classList.remove("hide")
+//     timeText.classList.remove("hide")
+//     shuffledQuestions = questions.sort(() => Math.random() - .5)
+//     currentQuestionIndex = 0
+//     setNextQuestion()
+// }
 
 function setNextQuestion(questions) {
     resetState()
@@ -62,12 +91,115 @@ function selectAnswer(e) {
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextbutton.classList.remove("hide")
     }else {
-        startbutton.innerText = "restart"
-        startbutton.classList.remove("hide")
+        finishButton.classList.remove("hide")
     }
 }
+// finishButton.addEventListener("click", endGame)
+function startGame(){
+    timer()
+    console.log("started")
+    startbutton.classList.add("hide")
+    questionContainterElement.classList.remove("hide")
+    timeText.classList.remove("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    setNextQuestion()
+}
 
-function setStatusClass (element, correct) {
+function endGame() {
+    finishButton.classList.add("hide")
+    questionElement.classList.add("hide")
+    answerButtonsElement.classList.add("hide")
+    endScreenElement.classList.remove("hide")
+    //stop timer, enter time value to scoreboard
+    //initials need to go to scoreboard
+        clearInterval(timerInterval);
+    finalScore.innerText = (secondsLeft)
+}
+submitButton.addEventListener("click", submitScore)
+function populateScoreboard() {
+  leaderBoard.innerHTML = ""
+  for (i = 0; i < scores.length; i++) {
+    var players = document.createElement("li")
+        players.innerText = scores[i][0]+", "+scores[i][1]
+        leaderBoard.appendChild(players)
+  }
+}
+function submitScore() {
+    endScreenElement.classList.add("hide")
+    submitButton.classList.add("hide")
+    scoreboardElement.classList.remove("hide")
+    scores.push([finalScore.innerHTML, userName.value])
+    scores.sort(function(a, b) {
+     return b[0] - a[0]
+    })
+    populateScoreboard()
+}
+function resetGame() {//NEED TO RESET STATES CORRECTLY TO BEGIN THE TEST AGAIN
+    submitButton.classList.remove("hide")
+    nextButton.classList.remove("hide")
+    questionElement.classList.remove("hide")
+    answerButtonsElement.classList.remove("hide")
+    endScreenElement.classList.add("hide")
+    scoreboardElement.classList.add("hide")
+    startGame()
+    secondsLeft = 60
+  }
+  
+  restartButton.addEventListener("click", resetGame) 
+  
+  resetButton.addEventListener("click", clearBoard)
+    function clearBoard() {
+      scores = []
+      populateScoreboard()
+    }
+  
+  highScorePage.addEventListener("click", showScores)
+    function showScores() {
+      scoreboardElement.classList.remove("hide")
+      questionElement.classList.add("hide")
+    answerButtonsElement.classList.add("hide")
+    }
+  
+// function startGame(){
+//     timer()
+//     console.log("started")
+//     startbutton.classList.add("hide")
+//     questionContainterElement.classList.remove("hide")
+//     timeText.classList.remove("hide")
+//     shuffledQuestions = questions.sort(() => Math.random() - .5)
+//     currentQuestionIndex = 0
+//     setNextQuestion()
+// }
+// function setStatusClass (element, correct) {
+//     clearStatusClass(element)
+//     if (correct){
+//         element.classList.add("correct")
+//     } else {
+//         element.classList.add("wrong")
+//     }
+// }
+// function clearStatusClass(element) {
+//     element.classList.remove("correct")
+//     element.classList.remove("wrong")
+// }
+//     function timer(){
+//     var counter = 80;
+//     setInterval(function() {
+//       counter--;
+//        if (counter >= 0) {
+//           span = document.getElementById("time");
+//           span.innerHTML = counter;
+//        }
+//        else {
+//         window.location.reload();
+//            alert ("sorry out of time.")
+//           clearInterval(counter);
+//         }
+//         timeText.textContent = counter;
+//       }, 1000);
+//  };
+ function setStatusClass (element, correct) {
     clearStatusClass(element)
     if (correct){
         element.classList.add("correct")
@@ -79,23 +211,6 @@ function clearStatusClass(element) {
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
-    function timer(){
-    var counter = 80;
-    setInterval(function() {
-      counter--;
-       if (counter >= 0) {
-          span = document.getElementById("time");
-          span.innerHTML = counter;
-       }
-       else {
-        window.location.reload();
-           alert ("sorry out of time.")
-          clearInterval(counter);
-        }
-        timeText.textContent = counter;
-      }, 1000);
- };
-
 //  first we need questions
 const questions =  [
      {
